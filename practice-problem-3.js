492602924b6d36afb7e84162392fd38dc94ab0dd
mashuk-tamim <tamimmashuk328@gmail.com>
2023-08-05 10:59:53 +0600

    practice problem 3 done

--- practice-problem-3.js
+++ practice-problem-3.js
@@ -1,1 +1,27 @@
+const prompt= require("prompt-sync")();
+
 function average(array)
+{
+    let sum=0, avg;
+    for(let i=0; i<array.length; i++)
+    {
+        sum=sum+array[i];
+    }
+    avg=sum/array.length;
+    console.log('sum ',sum);
+    return avg;
+}
+let n;
+n=prompt('Enter the size of the array:');
+
+let array=[];
+for(let i=0; i<n; i++)
+{
+    let inputValue = prompt('Enter array element:');
+    array[i] = Number(inputValue);
+}
+console.log(array);
+array2=[10,12,14];
+console.log(array2);
+let arrayAverage=average(array);
+console.log("The average is: ", arrayAverage);
